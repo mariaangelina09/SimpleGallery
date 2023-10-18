468f26e87fcaebf58681291dd7265efc14791da9
mangelina09 <linachan09@gmail.com>
2023-10-18 21:52:09 +0700

    Update Model Data

--- SimpleGallery/Model/ArtworkData.swift
+++ SimpleGallery/Model/ArtworkData.swift
@@ -7,24 +7,127 @@
 
 import Foundation
 
-struct ArtworkData: Codable {
-    let artworks: [Artwork]
-    
-    private enum CodingKeys: String, CodingKey {
-        case artworks
-    }
-    
-    init(artworks: [Artwork]) {
-        self.artworks = artworks
-    }
-    
-    init(from decoder:Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        artworks = try values.decode([Artwork].self, forKey: .artworks)
-    }
+//struct ArtworkData: Codable {
+//    let artworks: [Artwork]
+//    
+//    private enum CodingKeys: String, CodingKey {
+//        case artworks
+//    }
+//    
+//    init(artworks: [Artwork]) {
+//        self.artworks = artworks
+//    }
+//    
+//    init(from decoder:Decoder) throws {
+//        let values = try decoder.container(keyedBy: CodingKeys.self)
+//        artworks = try values.decode([Artwork].self, forKey: .artworks)
+//    }
+//}
+//
+//struct Artwork: Codable {
+//    let title: String
+//    let imageIdentifier: String
+//}
+
+// MARK: - ArtworkData
+struct ArtworkData: Decodable {
+    let pagination: Pagination
+    let data: [Artwork]
+    let info: Info
+    let config: Config
 }
 
-struct Artwork: Codable {
+// MARK: - Config
+struct Config: Decodable {
+    let iiifURL: String
+    let websiteURL: String
+}
+
+// MARK: - Artwork
+struct Artwork: Decodable {
+    let id: Int
     let title: String
-    let imageIdentifier: String
+    let thumbnail: Thumbnail
+    let artistDisplay, placeOfOrigin: String
+    let dimensionsDetail: [DimensionsDetail]
+    let mediumDisplay: String
+    let creditLine: String
+    let catalogueDisplay, publicationHistory, exhibitionHistory, provenanceText: String?
+    let edition: String?
+    let publishingVerificationLevel: PublishingVerificationLevel
+    let internalDepartmentID: Int
+    let copyrightNotice: String?
+    let color: Color
+    let onLoanDisplay: String?
+    let galleryTitle, galleryID, nomismaID: String?
+    let artworkTypeTitle: String
+    let artworkTypeID: Int
+    let departmentTitle, departmentID: String
+    let artistID: Int?
+    let artistTitle: String?
+    let termTitles: [String]
+    let classificationID, classificationTitle: String
+    let altClassificationIDS, classificationIDS, classificationTitles: [String]
+    let materialID: String?
+    let altMaterialIDS, materialIDS, materialTitles: [String]
+    let imageID: String
+    let suggestAutocompleteAll: [SuggestAutocompleteAll]
+    let sourceUpdatedAt, updatedAt, timestamp: Date
+}
+
+// MARK: - Color
+struct Color: Decodable {
+    let h, l, s: Int
+    let percentage: Double
+    let population: Int
+}
+
+// MARK: - DimensionsDetail
+struct DimensionsDetail: Decodable {
+    let depthCM, depthIn, widthCM, widthIn: Double
+    let heightCM, heightIn, diameterCM, diameterIn: Double
+    let clarification: String?
+}
+
+enum PublishingVerificationLevel: Decodable {
+    case webBasic
+    case webCataloged
+}
+
+// MARK: - SuggestAutocompleteAll
+struct SuggestAutocompleteAll: Decodable {
+    let input: [String]
+    let contexts: Contexts
+    let weight: Int?
+}
+
+// MARK: - Contexts
+struct Contexts: Decodable {
+    let groupings: [Grouping]
+}
+
+enum Grouping: Decodable {
+    case accession
+    case title
+}
+
+// MARK: - Thumbnail
+struct Thumbnail: Decodable {
+    let lqip: String
+    let width, height: Int?
+    let altText: String
+}
+
+// MARK: - Info
+struct Info: Decodable {
+    let licenseText: String
+    let licenseLinks: [String]
+    let version: String
+}
+
+// MARK: - Pagination
+struct Pagination: Decodable {
+    let total, limit, offset, totalPages: Int
+    let currentPage: Int
+    let nextURL: String
 }
--- SimpleGallery/Network/NetworkManager.swift
+++ SimpleGallery/Network/NetworkManager.swift
@@ -34,10 +34,54 @@ class NetworkManager {
 
             do {
                 let decoder = JSONDecoder()
-                let response = try decoder.decode(ArtworkData.self, from: data)
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                decoder.dateDecodingStrategy = .secondsSince1970
                 
-                let artworks = response.artworks.map { artworkData in
-                    return Artwork(title: artworkData.title, imageIdentifier: artworkData.imageIdentifier)
+                let artworkData = try decoder.decode(ArtworkData.self, from: data)
+                print(artworkData)
+                let artworks = artworkData.data.map { artwork in
+                    return Artwork(id: artwork.id,
+                                   title: artwork.title,
+                                   thumbnail: artwork.thumbnail,
+                                   artistDisplay: artwork.artistDisplay,
+                                   placeOfOrigin: artwork.placeOfOrigin,
+                                   dimensionsDetail: artwork.dimensionsDetail,
+                                   mediumDisplay: artwork.mediumDisplay,
+                                   creditLine: artwork.creditLine,
+                                   catalogueDisplay: artwork.catalogueDisplay,
+                                   publicationHistory: artwork.publicationHistory,
+                                   exhibitionHistory: artwork.exhibitionHistory,
+                                   provenanceText: artwork.provenanceText,
+                                   edition: artwork.edition,
+                                   publishingVerificationLevel: artwork.publishingVerificationLevel,
+                                   internalDepartmentID: artwork.internalDepartmentID,
+                                   copyrightNotice: artwork.copyrightNotice,
+                                   color: artwork.color,
+                                   onLoanDisplay: artwork.onLoanDisplay,
+                                   galleryTitle: artwork.galleryTitle,
+                                   galleryID: artwork.galleryID,
+                                   nomismaID: artwork.nomismaID,
+                                   artworkTypeTitle: artwork.artworkTypeTitle,
+                                   artworkTypeID: artwork.artworkTypeID,
+                                   departmentTitle: artwork.departmentTitle,
+                                   departmentID: artwork.departmentID,
+                                   artistID: artwork.artistID,
+                                   artistTitle: artwork.artistTitle,
+                                   termTitles: artwork.termTitles,
+                                   classificationID: artwork.classificationID,
+                                   classificationTitle: artwork.classificationTitle,
+                                   altClassificationIDS: artwork.altClassificationIDS,
+                                   classificationIDS: artwork.classificationIDS,
+                                   classificationTitles: artwork.classificationTitles,
+                                   materialID: artwork.materialID,
+                                   altMaterialIDS: artwork.altMaterialIDS,
+                                   materialIDS: artwork.materialIDS,
+                                   materialTitles: artwork.materialTitles,
+                                   imageID: artwork.imageID,
+                                   suggestAutocompleteAll: artwork.suggestAutocompleteAll,
+                                   sourceUpdatedAt: artwork.sourceUpdatedAt,
+                                   updatedAt: artwork.updatedAt,
+                                   timestamp: artwork.timestamp)
                 }
 
                 completion(artworks, nil)
@@ -71,8 +115,49 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 let response = try decoder.decode(ArtworkData.self, from: data)
                 
-                let artworks = response.artworks.map { artworkData in
-                    return Artwork(title: artworkData.title, imageIdentifier: artworkData.imageIdentifier)
+                let artworks = artworkData.data.map { artwork in
+                    return Artwork(id: artwork.id,
+                                   title: artwork.title,
+                                   thumbnail: artwork.thumbnail,
+                                   artistDisplay: artwork.artistDisplay,
+                                   placeOfOrigin: artwork.placeOfOrigin,
+                                   dimensionsDetail: artwork.dimensionsDetail,
+                                   mediumDisplay: artwork.mediumDisplay,
+                                   creditLine: artwork.creditLine,
+                                   catalogueDisplay: artwork.catalogueDisplay,
+                                   publicationHistory: artwork.publicationHistory,
+                                   exhibitionHistory: artwork.exhibitionHistory,
+                                   provenanceText: artwork.provenanceText,
+                                   edition: artwork.edition,
+                                   publishingVerificationLevel: artwork.publishingVerificationLevel,
+                                   internalDepartmentID: artwork.internalDepartmentID,
+                                   copyrightNotice: artwork.copyrightNotice,
+                                   color: artwork.color,
+                                   onLoanDisplay: artwork.onLoanDisplay,
+                                   galleryTitle: artwork.galleryTitle,
+                                   galleryID: artwork.galleryID,
+                                   nomismaID: artwork.nomismaID,
+                                   artworkTypeTitle: artwork.artworkTypeTitle,
+                                   artworkTypeID: artwork.artworkTypeID,
+                                   departmentTitle: artwork.departmentTitle,
+                                   departmentID: artwork.departmentID,
+                                   artistID: artwork.artistID,
+                                   artistTitle: artwork.artistTitle,
+                                   termTitles: artwork.termTitles,
+                                   classificationID: artwork.classificationID,
+                                   classificationTitle: artwork.classificationTitle,
+                                   altClassificationIDS: artwork.altClassificationIDS,
+                                   classificationIDS: artwork.classificationIDS,
+                                   classificationTitles: artwork.classificationTitles,
+                                   materialID: artwork.materialID,
+                                   altMaterialIDS: artwork.altMaterialIDS,
+                                   materialIDS: artwork.materialIDS,
+                                   materialTitles: artwork.materialTitles,
+                                   imageID: artwork.imageID,
+                                   suggestAutocompleteAll: artwork.suggestAutocompleteAll,
+                                   sourceUpdatedAt: artwork.sourceUpdatedAt,
+                                   updatedAt: artwork.updatedAt,
+                                   timestamp: artwork.timestamp)
                 }
                 
                 completion(artworks, nil)
