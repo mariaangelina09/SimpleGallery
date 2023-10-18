5e3d033f994e4776e6737528b6cb36f79dc88065
mangelina09 <linachan09@gmail.com>
2023-10-18 15:41:13 +0700

    Initial Commit

---
+++ SimpleGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleGallery
+//
+//  Created by Kristiawan Adi on 18/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
