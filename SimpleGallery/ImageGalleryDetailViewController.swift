2e71d1a9e885ea1fa766d457046ecf4f8a50c6f6
mangelina09 <linachan09@gmail.com>
2023-10-19 23:32:20 +0700

    Add Shadow

--- SimpleGallery/ImageGalleryDetailViewController.swift
+++ SimpleGallery/ImageGalleryDetailViewController.swift
@@ -14,7 +14,24 @@ class ImageGalleryDetailViewController: UIViewController {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
+        imageView.layer.shadowColor = UIColor.black.cgColor
+        imageView.layer.shadowOpacity = 0.5
+        imageView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        imageView.layer.shadowRadius = 4
+        imageView.layer.masksToBounds = false
         return imageView
+    }()
+    
+    let cardView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.2
+        view.layer.shadowOffset = CGSize(width: 0, height: 2)
+        view.layer.shadowRadius = 4
+        return view
     }()
     
     private lazy var titleLabel: UILabel = {
@@ -59,9 +76,10 @@ class ImageGalleryDetailViewController: UIViewController {
         }
         
         view.addSubview(imageView)
-        view.addSubview(titleLabel)
-        view.addSubview(subtitleLabel)
-        view.addSubview(categoryLabel)
+        view.addSubview(cardView)
+        cardView.addSubview(titleLabel)
+        cardView.addSubview(subtitleLabel)
+        cardView.addSubview(categoryLabel)
         
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
@@ -69,17 +87,22 @@ class ImageGalleryDetailViewController: UIViewController {
             imageView.widthAnchor.constraint(equalToConstant: 300),
             imageView.heightAnchor.constraint(equalToConstant: 300),
             
-            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            cardView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            cardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            titleLabel.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -20),
             
             subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
-            subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            subtitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            subtitleLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 20),
+            subtitleLabel.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -20),
             
             categoryLabel.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 20),
-            categoryLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            categoryLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            categoryLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 20),
+            categoryLabel.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -20),
+            categoryLabel.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -20),
         ])
         
         guard let imageURL = URL(string: "https://www.artic.edu/iiif/2/\(artworkDetail.imageID)/full/843,/0/default.jpg") else {
